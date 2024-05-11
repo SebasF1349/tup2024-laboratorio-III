b5448f2969a6cb90790e54f86871bb6bb333f433
SebasF1349 <ratablanca711@gmail.com>
2024-05-11 11:08:04 -0300

    fix: fix surname overwrite + move processor methods

--- src/main/java/ar/edu/utn/frbb/tup/view/BaseInputProcessor.java
+++ src/main/java/ar/edu/utn/frbb/tup/view/BaseInputProcessor.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frbb.tup.view;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class BaseInputProcessor {
@@ -9,5 +10,25 @@ public class BaseInputProcessor {
   protected static void clearScreen() {
     System.out.print("\033[H\033[2J");
     System.out.flush();
+  }
+
+  public String getStringInput(String question) {
+    System.out.println(question);
+    return scanner.nextLine();
+  }
+
+  public LocalDate getDateInput(String question) {
+    System.out.println(question + " (Formato: YYYY-MM-DD):");
+    LocalDate fecha = null;
+    boolean fechaValida = false;
+    while (!fechaValida) {
+      try {
+        fecha = LocalDate.parse(scanner.nextLine());
+        fechaValida = true;
+      } catch (Exception e) {
+        System.out.println("Formato de fecha inválido. Ingrese la fecha en formato YYYY-MM-DD:");
+      }
+    }
+    return fecha;
   }
 }
