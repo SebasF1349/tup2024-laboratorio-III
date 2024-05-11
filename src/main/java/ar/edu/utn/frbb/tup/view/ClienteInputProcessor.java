6169b8896d169dbc0d12d9bb8a618f75377edf1f
SebasF1349 <ratablanca711@gmail.com>
2024-05-11 11:08:04 -0300

    fix: fix surname overwrite + move processor methods

--- src/main/java/ar/edu/utn/frbb/tup/view/ClienteInputProcessor.java
+++ src/main/java/ar/edu/utn/frbb/tup/view/ClienteInputProcessor.java
@@ -7,26 +7,6 @@ import java.time.LocalDate;
 
 public class ClienteInputProcessor extends BaseInputProcessor {
 
-  private String getStringInput(String question) {
-    System.out.println(question);
-    return scanner.nextLine();
-  }
-
-  private LocalDate getDateInput(String question) {
-    System.out.println(question + " (Formato: YYYY-MM-DD):");
-    LocalDate fecha = null;
-    boolean fechaValida = false;
-    while (!fechaValida) {
-      try {
-        fecha = LocalDate.parse(scanner.nextLine());
-        fechaValida = true;
-      } catch (Exception e) {
-        System.out.println("Formato de fecha inválido. Ingrese la fecha en formato YYYY-MM-DD:");
-      }
-    }
-    return fecha;
-  }
-
   public Cliente ingresarCliente() {
 
     Cliente cliente = new Cliente();
@@ -43,30 +23,28 @@ public class ClienteInputProcessor extends BaseInputProcessor {
     cliente.setApellido(apellido);
 
     String calle = this.getStringInput("Ingrese la calle de la dirección del cliente:");
-    cliente.setApellido(calle);
     String numero = this.getStringInput("Ingrese el numero de la dirección del cliente:");
-    cliente.setApellido(numero);
     String departamento = this.getStringInput("Ingrese el número de departamento del cliente:");
-    cliente.setApellido(departamento);
-    Direccion direccionCliente = new Direccion(calle, numero, departamento);
+    String ciudad = this.getStringInput("Ingrese la ciudad de residencia del cliente:");
+    Direccion direccionCliente = new Direccion(calle, numero, departamento, ciudad);
     cliente.setDireccion(direccionCliente);
 
     String nroTelefono = this.getStringInput("Ingrese el teléfono del cliente:");
     cliente.setNroTelefono(nroTelefono);
 
-    LocalDate fechaNacimiento = this.getDateInput("Ingrese la fecha de nacimiento del cliente");
+    LocalDate fechaNacimiento = this.getDateInput("Ingrese la fecha de nacimiento del cliente:");
     cliente.setFechaNacimiento(fechaNacimiento);
 
-    System.out.println("Ingrese el tipo de persona Física(F) o Jurídica(J):");
+    System.out.println("Ingrese el tipo de persona Física[F] o Jurídica[J]:");
     String tipoPersonaStr = scanner.nextLine().toUpperCase();
-    while (!tipoPersonaStr.equals("F") && !tipoPersonaStr.equals("J")) {
+    while (!tipoPersonaStr.equalsIgnoreCase("F") && !tipoPersonaStr.equalsIgnoreCase("J")) {
       System.out.println("Tipo de persona inválido. Ingrese F (FÍSICA) o J (JURIDICA):");
       tipoPersonaStr = scanner.nextLine().toUpperCase();
     }
     TipoPersona tipoPersona = TipoPersona.fromString(tipoPersonaStr);
     cliente.setTipoPersona(tipoPersona);
 
-    LocalDate fechaAlta = this.getDateInput("Ingrese la fecha de alta del cliente");
+    LocalDate fechaAlta = this.getDateInput("Ingrese la fecha de alta del cliente:");
     cliente.setFechaAlta(fechaAlta);
 
     clearScreen();
