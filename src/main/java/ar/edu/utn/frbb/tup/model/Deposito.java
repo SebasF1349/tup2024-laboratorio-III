83aaa6a7880af09c75564ed9f759d2d1b374456c
SebasF1349 <ratablanca711@gmail.com>
2024-05-12 14:25:20 -0300

    chore: add Movimiento constructor

--- src/main/java/ar/edu/utn/frbb/tup/model/Deposito.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/Deposito.java
@@ -1,6 +1,10 @@
 package ar.edu.utn.frbb.tup.model;
 
 public class Deposito extends Movimiento {
+  public Deposito(double monto) {
+    super(monto);
+  }
+
   public TipoMovimiento getTipoMovimiento() {
     return TipoMovimiento.DEBITO;
   }
