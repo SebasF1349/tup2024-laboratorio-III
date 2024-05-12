0d674483ec4e999cf67579b23d3ecdbf54d96319
SebasF1349 <ratablanca711@gmail.com>
2024-05-12 14:40:08 -0300

    chore: cleaning

--- src/main/java/ar/edu/utn/frbb/tup/view/CuentaCreateProcessor.java
+++ src/main/java/ar/edu/utn/frbb/tup/view/CuentaCreateProcessor.java
@@ -1,24 +1,10 @@
 package ar.edu.utn.frbb.tup.view;
 
-import ar.edu.utn.frbb.tup.model.Banco;
-import ar.edu.utn.frbb.tup.model.Cliente;
 import ar.edu.utn.frbb.tup.model.Cuenta;
 import ar.edu.utn.frbb.tup.model.MonedaCuenta;
 import ar.edu.utn.frbb.tup.model.TipoCuenta;
-import java.util.Objects;
 
 public class CuentaCreateProcessor extends BaseInputProcessor {
-  public Cliente modifyClienteByDni(Banco banco) {
-    String dni = this.getStringInput("Ingrese el dni del Clienta asociado a la Cuenta:");
-    Cliente cliente = banco.getClienteByDni(dni);
-    if (Objects.isNull(cliente)) {
-      System.out.println("Cliente no encontrado.");
-      scanner.nextLine();
-      return null;
-    }
-    return cliente;
-  }
-
   public Cuenta createCuenta() {
     clearScreen();
 
