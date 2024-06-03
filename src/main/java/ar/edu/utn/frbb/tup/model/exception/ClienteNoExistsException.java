8001efabc3269acb9fbfa3681c1e439c37408feb
SebasF1349 <ratablanca711@gmail.com>
2024-06-02 23:24:44 -0300

    fix: missing exception

---
+++ src/main/java/ar/edu/utn/frbb/tup/model/exception/ClienteNoExistsException.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frbb.tup.model.exception;
+
+public class ClienteNoExistsException extends Throwable {
+  public ClienteNoExistsException(String message) {
+    super(message);
+  }
+}
