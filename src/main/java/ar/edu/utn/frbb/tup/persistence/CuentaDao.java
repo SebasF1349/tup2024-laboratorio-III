920660fc7c2d9643c091d0d1840c22bbd0889bf3
SebasF1349 <ratablanca711@gmail.com>
2024-06-06 14:32:40 -0300

    feat: add CuentaDao.getCuentasByCliente

--- src/main/java/ar/edu/utn/frbb/tup/persistence/CuentaDao.java
+++ src/main/java/ar/edu/utn/frbb/tup/persistence/CuentaDao.java
@@ -2,6 +2,8 @@ package ar.edu.utn.frbb.tup.persistence;
 
 import ar.edu.utn.frbb.tup.model.Cuenta;
 import ar.edu.utn.frbb.tup.persistence.entity.CuentaEntity;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,5 +21,16 @@ public class CuentaDao extends AbstractBaseDao {
   public void save(Cuenta cuenta) {
     CuentaEntity entity = new CuentaEntity(cuenta);
     getInMemoryDatabase().put(entity.getId(), entity);
+  }
+
+  public List<Cuenta> getCuentasByCliente(String dni) {
+    List<Cuenta> cuentasDelCliente = new ArrayList<>();
+    for (Object object : getInMemoryDatabase().values()) {
+      CuentaEntity cuenta = ((CuentaEntity) object);
+      if (cuenta.getTitular().equals(dni)) {
+        cuentasDelCliente.add(cuenta.toCuenta());
+      }
+    }
+    return cuentasDelCliente;
   }
 }
