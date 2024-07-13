5c4edd61e673b4f93ddb114f2f2ef1de140ff255
SebasF1349 <ratablanca711@gmail.com>
2024-07-13 15:23:18 -0300

    feat: create SpringBoot app

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # tup2024
 
-execute with `java -cp target/sistema-bancario-1.0-SNAPSHOT-shaded.jar ar.edu.utn.frbb.tup.Application`
-
-`mvn exec:java`
+To run: `mvn spring-boot:run`
--- src/main/java/ar/edu/utn/frbb/tup/Application.java
+++ src/main/java/ar/edu/utn/frbb/tup/Application.java
@@ -1,25 +1,12 @@
 package ar.edu.utn.frbb.tup;
 
-import ar.edu.utn.frbb.tup.presentation.input.MenuInputProcessor;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
   public static void main(String args[]) {
-    try (ConfigurableApplicationContext applicationContext =
-        new AnnotationConfigApplicationContext(ApplicationConfig.class)) {
-      MenuInputProcessor menuInputProcessor = applicationContext.getBean(MenuInputProcessor.class);
-      menuInputProcessor.renderMenu();
-    } catch (BeansException e) {
-      e.printStackTrace();
-    }
-
-    // ConfigurableApplicationContext applicationContext =
-    //    new AnnotationConfigApplicationContext(ApplicationConfig.class);
-    //
-    // MenuInputProcessor menuInputProcessor = applicationContext.getBean(MenuInputProcessor.class);
-    // menuInputProcessor.renderMenu();
+    SpringApplication.run(Application.class, args);
   }
 }
