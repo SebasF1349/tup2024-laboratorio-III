7397f70a70b21fb04a49a03125a60460882bf6cb
SebasF1349 <ratablanca711@gmail.com>
2024-07-13 16:59:24 -0300

    chore: remove processors

--- src/main/java/ar/edu/utn/frbb/tup/presentation/input/ClienteProcessor.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frbb.tup.presentation.input;
-
-import ar.edu.utn.frbb.tup.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class ClienteProcessor extends BusinessProcessor {
-  @Autowired ClienteService clienteService = new ClienteService();
-}
