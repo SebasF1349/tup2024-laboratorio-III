2438cb6a478c0143c1dbe6d56929378d26c515eb
SebasF1349 <ratablanca711@gmail.com>
2024-07-13 18:07:57 -0300

    fix: missing new Exception

---
+++ src/main/java/ar/edu/utn/frbb/tup/controller/validator/WrongInputDataException.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frbb.tup.controller.validator;
+
+public class WrongInputDataException extends Exception {
+  public WrongInputDataException(String message) {
+    super(message);
+  }
+}
