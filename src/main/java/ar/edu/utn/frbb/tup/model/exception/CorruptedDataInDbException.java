5d170bf4b48fd1ab1846321f128503c8240c62e5
SebasF1349 <ratablanca711@gmail.com>
2024-08-04 17:16:14 -0300

    feat: add logging to CorruptedDataInDbException to not expose data

--- src/main/java/ar/edu/utn/frbb/tup/model/exception/CorruptedDataInDbException.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/exception/CorruptedDataInDbException.java
@@ -1,7 +1,13 @@
 package ar.edu.utn.frbb.tup.model.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CorruptedDataInDbException extends InternalServerErrorException {
+  private static final Logger logger = LoggerFactory.getLogger(CorruptedDataInDbException.class);
+
   public CorruptedDataInDbException(String message) {
-    super(message, 101);
+    super("No pudo realizarse la operación por un problema interno", 101);
+    logger.error(message);
   }
 }
