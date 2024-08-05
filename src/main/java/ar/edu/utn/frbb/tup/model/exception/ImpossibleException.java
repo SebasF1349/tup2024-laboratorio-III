b577f7cbbf2e6db54ae25b44ba28d77b12cabdf2
SebasF1349 <ratablanca711@gmail.com>
2024-08-05 09:06:51 -0300

    chore: move logger to ServerException

--- src/main/java/ar/edu/utn/frbb/tup/model/exception/ImpossibleException.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/exception/ImpossibleException.java
@@ -1,13 +1,9 @@
 package ar.edu.utn.frbb.tup.model.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ImpossibleException extends InternalServerErrorException {
-  private static final Logger logger = LoggerFactory.getLogger(ImpossibleException.class);
 
   public ImpossibleException(String message) {
     super("Error irrecuperable", 100);
-    logger.error(message);
+    logger.error("Impossible Exception: " + message);
   }
 }
--- src/main/java/ar/edu/utn/frbb/tup/model/exception/ServerException.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/exception/ServerException.java
@@ -1,6 +1,11 @@
 package ar.edu.utn.frbb.tup.model.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class ServerException extends BaseCustomException {
+  protected final Logger logger = LoggerFactory.getLogger(ImpossibleException.class);
+
   public ServerException(String message, Integer statusCode, Integer internalCode) {
     super(message, statusCode, internalCode);
   }
